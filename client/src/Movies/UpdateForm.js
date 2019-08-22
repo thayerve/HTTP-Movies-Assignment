04593427b27aa61362df86c04f04eb842a2b0636
Virginia Thayer <virginia@stripe.com>
2019-08-22 14:58:27 -0700

    implemented working PUT request and pushing user back to home (movie list) on successful update.

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const emptyMovie = {
-    director: "",
-    id: "",
-    metascore: "",
-    stars: [],
-    title: ""
-}
+
 
 export default function UpdateForm(props) {
-    console.log('props to UpdateForm: ', props);
-
-    const [movie, setMovie] = useState(emptyMovie);
+    console.log('UpdateForm rendered');
     const id = props.match.params.id;
-
+    const emptyMovie = {
+        director: "",
+        id: id,
+        metascore: "",
+        stars: [],
+        title: ""
+    };
+    const [movie, setMovie] = useState(emptyMovie);
+    
     useEffect(() => {
         axios
             .get(`http://localhost:5000/api/movies/${id}`)
@@ -32,47 +32,59 @@ export default function UpdateForm(props) {
             ...movie,
             [e.target.name]: e.target.value
         });
+        console.log('current value of movie: ', movie);
     };
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log('handleSubmit function called. Event: ', e);
-    //     axios
-    //   .put('')
-    //   .then(res)
-    //   .catch(err)
+        console.log('handleSubmit function called.');
+        // console.log(typeof movie.stars);
+        // if(typeof movie.stars === "string") {
+        //     const newStarsArray = movie.stars.split(",");
+        //     setMovie({...movie, stars: newStarsArray});
+        // };
+        // ^ WIP trying to make movie.stars back into an array after editing
+        axios
+            .put('http://localhost:5000/api/movies/${id}', movie)
+            .then(res => {
+                console.log('PUT response: ', res.data);
+                props.history.push('/');
+            })
+            .catch(err => console.log(err.response));
+
+        
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <input
-          type="text"
-          name="title"
-          onChange={handleChange}
-          placeholder="Title"
-          value={movie.title}
-        />
-        <input
-          type="text"
-          name="director"
-          onChange={handleChange}
-          placeholder="Director"
-          value={movie.director}
-        />
-         <input
-          type="text"
-          name="metascore"
-          onChange={handleChange}
-          placeholder="Metascore"
-          value={movie.metascore}
-          />
-          <input
-          type="text"
-          name="stars"
-          onChange={handleChange}
-          placeholder="Stars"
-          value={movie.stars}
-          />
+                type="text"
+                name="title"
+                onChange={handleChange}
+                placeholder="Title"
+                value={movie.title}
+            />
+            <input
+                type="text"
+                name="director"
+                onChange={handleChange}
+                placeholder="Director"
+                value={movie.director}
+            />
+            <input
+                type="number"
+                name="metascore"
+                onChange={handleChange}
+                placeholder="Metascore"
+                value={movie.metascore}
+            />
+            <input
+                type="text"
+                name="stars"
+                onChange={handleChange}
+                placeholder="Stars"
+                value={movie.stars}
+            />
 
             <button>Submit</button>
         </form>
